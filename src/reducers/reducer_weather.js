dbd4b7d4b8113cd9c8fac49a42585cb1c3547f3d
Sean Imam <seanimamo@gmail.com>
2018-09-09 22:03:12 -0400

    reducer now handles 404

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,15 +3,13 @@ import {FETCH_WEATHER} from '../actions/action_fetchWeather';
 //note we want the weather prop to be an array so we default it to []
 export default function(state = [], action){
     
-    if (!action.payload === '404'){
         switch(action.type){
             case FETCH_WEATHER:
-                if (action.payload.data.cod === '200'){
-                //console.log('Action Recieved', action);
+                if (action.payload != '404' && action.payload.data.cod === '200'){
+                console.log('Action Recieved', action);
                 //notice we always return a new array for state and never directly modify the current state object. (you're supposed to use this.setState)
                     return [action.payload.data, ...state];  
                 }
         }
-    } 
     return state;
 }
