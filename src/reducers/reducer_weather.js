e91f61591ffebacf699eb1ebbf9da3dea0e0b37e
Sean Imam <seanimamo@gmail.com>
2018-09-09 20:53:38 -0400

    reducer typo fix

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,7 +5,7 @@ export default function(state = [], action){
    
     switch(action.type){
         case FETCH_WEATHER:
-            if (action.payload.data.cod = '200'){
+            if (action.payload.data.cod == '200'){
             //console.log('Action Recieved', action);
             //notice we always return a new array for state and never directly modify the current state object. (you're supposed to use this.setState)
                 return [action.payload.data, ...state];  
