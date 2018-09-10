2aeaf90cc1bd325ebcfd2c47497dd9a947bc8b59
Sean Imam <seanimamo@gmail.com>
2018-09-09 22:05:13 -0400

    weather api switched to https

--- src/actions/action_fetchWeather.js
+++ src/actions/action_fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '754d52791bf93f1447b945283be70d60';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
